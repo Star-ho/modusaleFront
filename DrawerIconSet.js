92becce0e4dfd73ca5b0865f3092a418c45f8fcd
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-10-20 00:50:51 +0900

    색넣기!

--- DrawerIconSet.js
+++ DrawerIconSet.js
@@ -5,10 +5,10 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function DrawerIcon ({val}) {
     if(val==1){
-        return <View flexDirection="row"><Text allowFontScaling={false} >이름순</Text></View>
+        return <View flexDirection="row"><Text allowFontScaling={false} style={{color:"#7f7a7a"}} >이름순</Text></View>
     }else if(val==2){
-        return <View flexDirection="row"><Text allowFontScaling={false} >이름역순</Text></View>
+        return <View flexDirection="row"><Text allowFontScaling={false} style={{color:"#7f7a7a"}} >이름역순</Text></View>
     }else{
-        return <View flexDirection="row"><Text allowFontScaling={false} >할인금액순</Text></View>
+        return <View flexDirection="row"><Text allowFontScaling={false} style={{color:"#7f7a7a"}} >할인금액순</Text></View>
     }
 }
