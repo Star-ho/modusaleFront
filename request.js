e60996fbb7ed187309e96b323f348be0a3237034
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-02-17 23:37:13 +0900

    요청 버전 수정

--- request.js
+++ request.js
@@ -1,8 +1,8 @@
 
 export async function getAppDataRequest(location){
     return location?fetch(`http://sailmoa.com/getDataFromGps?latitude=${location.coords.latitude}&longitude=${location.coords.longitude}`).then(res=>res.json()):
-      fetch("http://sailmoa.com/?ver=0.92").then(res=>res.json())
+      fetch("http://sailmoa.com/?ver=0.93").then(res=>res.json())
 
     // return location?fetch(`http://192.168.1.171/getDataFromGps?latitude=${location.coords.latitude}&longitude=${location.coords.longitude}`).then(res=>res.json())
-    // :fetch("http://192.168.1.171/?ver=0.92").then(res=>res.json())
+    // :fetch("http://192.168.1.171/?ver=0.93").then(res=>res.json())
 }
